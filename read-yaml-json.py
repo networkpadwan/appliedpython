ee5e812e6bffebfdaf806bebead9ae06ef6ff8b1
Sahil <sahil.pujani@gmail.com>
2015-08-20 17:01:12 -0700

    Completed exercise 1

---
+++ read-yaml-json.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import json, yaml
+print "This program will read from YML and JSON files."
+print "It will also pretty print the data structure that is returned from the files"
+
+#fetching info from YML files
+yml_file_name = raw_input("Please enter the YAML file name: ")
+#print yml_file_name
+
+with open(yml_file_name) as f:
+    yml_list = yaml.load(f)
+
+print yml_list
+#fetching info from JSON file
+json_file_name = raw_input("Please enter the JSON file name: ")
+#print json_file_name
+with open(json_file_name) as fout:
+    json_list = json.load(fout)
+
+print json_list
+
+
